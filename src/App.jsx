42d0028e61bbea573576c44e3e907dbd0f60f462
GABRIEL <cg3026434@labA204m33.IFSP>
2024-04-29 10:27:24 -0300

    Ampliando  Fotos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import FotoAmpliada from './components/FotoAmpliada';
 import FotoList from './components/FotoList';
 import SearchBar from './components/SearchBar';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import FotoAmpliada from './components/FotoAmpliada';
 
 function App() {  
   const [query, setQuery] = useState('');  
   const [categoria, setCategoria] = useState('');
   const [fotos, setFotos] = useState([]);
+  const [fotoAmpliada, setFotoAmpliada] = useState(null);
 
   const fetchData = async ({ query, categoria }) => {
     const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
@@ -18,8 +19,6 @@ function App() {
       },
     });
     setFotos(response.data);
-    console.log(response.data);
-    console.log(response);  
   };
 
   useEffect(() => {
@@ -27,16 +26,12 @@ function App() {
   }, []);
 
   return (
-    <>
       <div className="container">
         <SearchBar/>
-        <FotoList fotos={fotos}/>
-        <FotoAmpliada/>
+        <FotoList fotos={fotos} setFotoAmpliada={setFotoAmpliada}/>
+        <FotoAmpliada fotos={fotoAmpliada} setFotoAmpliada={setFotoAmpliada}/>
       </div>
-    </>
   )
-
-
 }
 
 export default App
--- src/components/FotoList.jsx
+++ src/components/FotoList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Foto from './Foto';
 
-const FotoList = ({ fotos }) => {
+const FotoList = ({ fotos, setFotoAmpliada }) => {
   return (
     <div className="album">
       {fotos.map((foto) => (
-        <Foto key={foto.id} dados={foto}/>
+        <Foto key={foto.id} dados={foto} setFotoAmpliada={setFotoAmpliada}/>
       ))};
     </div>
   );
 }
 
-export default FotoList;    
+export default FotoList;
